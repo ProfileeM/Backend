dc833d8b4f692328ec646e8db83b7cd4eca13a64
0lYUMA <102151689+0lYUMA@users.noreply.github.com>
2023-08-16 20:50:45 +0900

    first commit

---
+++ src/test/java/com/example/profileem/ProfileeMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.profileem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileeMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
