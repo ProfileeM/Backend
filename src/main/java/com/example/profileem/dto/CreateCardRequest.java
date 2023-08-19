1b5a29c22db06309ae2b77395ebf5ff2fc5faf14
2018079116 <wjdwlstjd968@naver.com>
2023-08-19 15:35:27 +0900

    test: 배포 test

---
+++ src/main/java/com/example/profileem/dto/CreateCardRequest.java
@@ -0,0 +1,10 @@
+package com.example.profileem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+public class CreateCardRequest {
+    // 객체 생성
+}
