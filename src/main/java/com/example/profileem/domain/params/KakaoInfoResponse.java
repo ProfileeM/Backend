86b78220648cdca16edf2923476cd1774726e123
0lYUMA <102151689+0lYUMA@users.noreply.github.com>
2023-10-09 14:35:31 +0900

    feat: kakao login

---
+++ src/main/java/com/example/profileem/domain/params/KakaoInfoResponse.java
@@ -0,0 +1,24 @@
+package com.example.profileem.domain.params;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+public class KakaoInfoResponse {
+
+    private Long id;
+    private Properties properties;
+    private KakaoAccount kakao_account;
+
+    @Getter
+    @ToString
+    public static class KakaoAccount {
+        private String email;
+    }
+
+    @Getter
+    @ToString
+    public static class Properties {
+        private String nickname;
+    }
+}
---
+++ src/main/java/com/example/profileem/jwt/JwtAuthenticationProvider.java
@@ -0,0 +1,68 @@
+package com.example.profileem.jwt;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtAuthenticationProvider {
+
+    @Value("${jwt.secret}")
+    private static String secretKey;
+
+    @Value("${jwt.access-token-time}")
+    private static long access_token_time;
+
+    @Value("${jwt.refresh-token-time}")
+    private static long refresh_token_time;
+
+
+    // Access Token 생성
+    public String createAccessToken(Long userId, String name) {
+        return createToken(userId, name, "Access", access_token_time);
+    }
+
+    // Refresh Token 생성
+    public String createRefreshToken(Long userId, String name) {
+        return createToken(userId, name, "Refresh", refresh_token_time);
+    }
+
+    public static String createToken(Long userId, String name,
+                                     String type, Long tokenValidTime) {
+        return Jwts.builder()
+                .setHeaderParam("type", type) // Header 구성
+                .setClaims(createClaims(userId, name)) // Payload - Claims 구성
+                .setSubject(userId.toString()) // Payload - Subject 구성
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .setExpiration(new Date(System.currentTimeMillis() + tokenValidTime))
+                .compact();
+    }
+
+    public static Claims createClaims(Long userId, String name) {
+        Claims claims = Jwts.claims();
+        claims.put("userId", userId);
+        claims.put("name", name);
+        return claims;
+    }
+
+    public String getPayload(String token) {
+        try {
+            return Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getSubject();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims().getSubject();
+        } catch (JwtException e) {
+            throw new RuntimeException("유효하지 않은 토큰입니다.");
+        }
+    }
+
+    public boolean validateToken(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
+                .getBody().getExpiration().before(new Date());
+    }
+}
